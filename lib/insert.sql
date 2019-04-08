acaf1a504dc01792dc01b87b346c2d05b1006074
Matthew Bartl <mattbartl@gmail.com>
2019-04-08 20:36:25 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Mr. Chocolate', 14, 'male', 'brown', 'angry');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Rowdy', 8, 'male', 'alaskan', 'happy');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Tabitha', 4, 'female', 'black', 'friendly');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Sergeant Brown', 9, 'male', 'brown', 'timid');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Melissa', 6, 'female', 'alaskan', 'angry');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Grinch', 9, 'male', 'black', 'timid');
+
+INSERT INTO bears (name, age, gender, color, temperament)
+VALUES ('Wendy', 7, 'female', 'brown', 'friendly');
+
+INSERT INTO bears (age, gender, color, temperament)
+VALUES (6, 'male', 'alaskan', 'angry');
